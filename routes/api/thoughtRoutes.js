eb10241e5be7fe040e796dc7309f8e45fed49543
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-08-28 21:23:28 +0100

    thought routes in progress

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const {} = require("../../controllers/thoughtController.js");
+
+// /api/thoughts
+router.route("/").get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+module.exports = router;
