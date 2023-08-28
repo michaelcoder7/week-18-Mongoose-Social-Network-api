ac0f47a8ed61a29a347d8c6e2b0b44685077baf2
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-08-28 20:46:21 +0100

    user model completed

--- models/User.js
+++ models/User.js
@@ -7,8 +7,56 @@ var validateEmail = function (email) {
 };
 
 // Schema to create User model
-const userSchema = new Schema({});
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: [validateEmail, "Please fill a valid email address"],
+      match: [
+        /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
+// Create a virtual property `fullName` that gets and sets the user's full name
+userSchema
+  .virtual("friendCount")
+  // Getter
+  .get(function () {
+    return `${this.friends.length}`;
+  });
+
+// Initialize our User model
 const User = model("user", userSchema);
 
 module.exports = User;
