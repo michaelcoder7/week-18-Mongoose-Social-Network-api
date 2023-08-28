d6842974ff8fe68382bbd1e8ae1ddda86073e898
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-08-28 21:00:24 +0100

    reaction model completed

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,6 +1,33 @@
 const { Schema, Types } = require("mongoose");
 const formatDate = require("../utils/date");
 
-const reactionSchema = new Schema({});
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: formatDate,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 module.exports = reactionSchema;
