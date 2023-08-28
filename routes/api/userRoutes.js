e5338146c4e74fd2e5c92288fb584ee6ae4ba994
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-08-28 21:19:06 +0100

    user routes completed

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,5 +1,13 @@
 const router = require("express").Router();
-const {} = require("../../controllers/userController");
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  updateUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/userController");
 
 // /api/users
 router.route("/").get(getUsers).post(createUser);
