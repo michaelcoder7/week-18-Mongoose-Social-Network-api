19c196c08698657d0272df21998d9ce6b89a2563
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-08-29 05:55:50 +0100

    data updated

--- utils/data.js
+++ utils/data.js
@@ -74,41 +74,78 @@ const names = [
   "Parker",
 ];
 
-const appDescriptions = [
-  "Decision Tracker",
-  "Find My Phone",
-  "Learn Piano",
-  "Starbase Defender",
-  "Tower Defense",
-  "Monopoly Money Manager",
-  "Movie trailers",
-  "Hello world",
-  "Stupid Social Media App",
-  "Notes",
-  "Messages",
-  "Email",
-  "Compass",
-  "Firefox",
-  "Running app",
-  "Cooking app",
-  "Poker",
-  "Deliveries",
+const thoughtDescriptions = [
+  "Just had the best cup of coffee ever!",
+  "Enjoying a beautiful sunset at the beach.",
+  "Just got the new XYZ smartphone. Excited to try it out.",
+  "Just booked a trip to Hawaii!",
+  "Tried making homemade pasta today, and it turned out great!",
+  "Going for a morning run.",
+  "Looking for recommendations for things to do in Hawaii.",
+  "Had a great time at the beach today.",
+  "The camera on the new XYZ smartphone is amazing.",
+  "Nothing beats a morning run. ",
+  "Packing for my trip to Hawaii. Can't wait!",
+  "The waves were amazing today.",
+  "The new XYZ smartphone has some really cool features.",
+  "Making homemade pizza for dinner tonight.",
+  "Just completed a 5k run.",
+  "Off to Hawaii!",
+  "Found a beautiful seashell at the beach.",
+  "Just posted a review of the new XYZ smartphone on my blog.",
+  "The homemade pizza turned out delicious!",
+  "Planning to run a marathon next month.",
 ];
+
+const possibleReactions = [
+  "Like",
+  "Love",
+  "Haha",
+  "Wow",
+  "Sad",
+  "Angry",
+  "This made my day!",
+  "So inspiring!",
+  "I can relate to this.",
+  "This is hilarious.",
+  "This is heartbreaking.",
+  "Great post!",
+  "Interesting, I hadn't thought about it that way.",
+  "Thanks for sharing.",
+  "Keep up the good work.",
+];
+
+const users = [];
 
 // Get a random item given an array
 const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 // Gets a random full name
-const getRandomName = () =>
-  `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
+const getRandomName = () => `${getRandomArrItem(names)}`;
 
-// Function to generate random assignments that we can add to student object.
-const getRandomAssignments = (int) => {
+// Function to generate random thoughts that we can add to the database. Includes thought reactions.
+const getRandomThoughts = (int) => {
+  let results = [];
+  for (let i = 0; i < int; i++) {
+    results.push({
+      thoughtText: getRandomArrItem(thoughtDescriptions),
+      username: getRandomArrItem(names),
+      reactions: [...getThoughtReactions(3)],
+    });
+  }
+  return results;
+};
+
+// Create the reactions that will be added to each thought
+const getThoughtReactions = (int) => {
+  if (int === 1) {
+    return getRandomArrItem(possibleReactions);
+  }
   const results = [];
   for (let i = 0; i < int; i++) {
     results.push({
-      assignmentName: getRandomArrItem(appDescriptions),
-      score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
+      reactionBody: getRandomArrItem(possibleReactions),
+      username: getRandomName(),
     });
   }
   return results;
