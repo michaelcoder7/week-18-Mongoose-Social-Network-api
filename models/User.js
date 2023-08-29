dd1ee37bdd17e225b7068a83d115b3e56478a960
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-08-29 09:27:49 +0100

    user updated

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require("mongoose");
-// const Thought = require("./Thought");
+const Thought = require("./Thought");
 
 var validateEmail = function (email) {
   var re = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
